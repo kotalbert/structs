198e8fb16aff1e924bbdb7d438bfb8b1271f6276
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2024-01-04 22:38:18 +0100

    Add person example

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type person struct {
+	firstName string
+	lastName  string
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	alex := person{firstName: "Alex", lastName: "DeLarge"}
+	var billie person
+
+	billie.firstName = "Billy"
+	billie.lastName = "Boy"
+
+	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
+	fmt.Printf("%+v", billie)
 }
