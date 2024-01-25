69886556170822539cd63a02fa097c26fb22c247
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2024-01-25 20:58:24 +0100

    Move struct to separate package

--- main.go
+++ main.go
@@ -1,50 +1,23 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"kotalbert/structs/user"
 	"log"
-	"time"
 )
-
-type user struct {
-	firstName string
-	lastName  string
-	birthDate string
-	createdAt time.Time
-}
-
-// Constructor
-// Input validation
-func newUser(firstName string, lastName string, birthDate string) (*user, error) {
-	if firstName == "" || lastName == "" || birthDate == "" {
-		return nil, errors.New("firstname, lastname, birthdate are expecting non empty string")
-	}
-
-	return &user{
-		firstName,
-		lastName,
-		birthDate,
-		time.Now(),
-	}, nil
-}
-
-func (u user) outputUserData() {
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
-}
 
 func main() {
 	fn := getUserData("Please enter your first name: ")
 	ln := getUserData("Please enter your last name: ")
 	bd := getUserData("Please enter your bd (MM/DD/YYYY): ")
 
-	appUser, err := newUser(fn, ln, bd)
+	appUser, err := user.NewUser(fn, ln, bd)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	appUser.outputUserData()
+	appUser.OutputUserData()
 
 }
 
